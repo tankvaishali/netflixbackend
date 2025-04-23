668a848e2b54e8fdcfae5a043f27fde586836d40
surbhi <surbhipansuriya633@gmail.com>
2025-04-23 11:53:05 +0530

    add episode, season, series in app.js file

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import MongoDb from './MongoDB/Connection.js';
 import EpisodeUpload from './APIs/EpisodeUploads.js';
 import register from './APIs/Registration.js';
 import adminlogin from './APIs/AdminpanelLogin.js';
+import AddSeason from './APIs/AddSeason.js';
+import AddEpisode from './APIs/AddEpisode.js';
+import AddSeries from './APIs/AddSeries.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,8 +22,11 @@ MongoDb();
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // http://localhost:8000/uploads
-app.use("/",adminlogin)
+app.use("/", adminlogin)
 app.use("/", EpisodeUpload);
-app.use("/",register)
+app.use("/", register)
+app.use("/", AddSeason)
+app.use("/", AddEpisode)
+app.use("/", AddSeries)
 
 app.listen(8000, () => console.log("Server running on port 8000"));
