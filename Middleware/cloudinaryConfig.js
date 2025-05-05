b5b94c9d6700fc4e508d2c6e60810386d5d1827a
surbhi <surbhipansuriya633@gmail.com>
2025-05-05 10:39:55 +0530

    create cloudinaryconfig file

---
+++ Middleware/cloudinaryConfig.js
@@ -0,0 +1,10 @@
+// Middleware/cloudinaryConfig.js
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'dkly9is4l',
+    api_key: '616468264798862',
+    api_secret: 'le9IHXRPueawjJf8VjWd57vO69M',
+});
+
+export default cloudinary;
