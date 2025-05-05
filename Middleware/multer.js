f0e9beaf288846bb58ebf858a4323c6db00c92f5
tankvaishali <vaishalitank28603@gmail.com>
2025-05-05 10:44:58 +0530

    change multer file

--- Middleware/multer.js
+++ Middleware/multer.js
@@ -1,19 +1,40 @@
+// import multer from 'multer';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// // ✅ Use absolute path
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, '../uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     },
+// });
+
+// const multerupload = multer({ storage });
+
+// export default multerupload;
+
+
+
+
+
 import multer from 'multer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from './cloudinaryConfig.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// ✅ Use absolute path
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../uploads'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    },
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'uploads',
+        allowed_formats: ['jpg', 'png', 'jpeg', 'mp4', 'mov'],
+        transformation: [{ quality: 'auto' }, { fetch_format: 'auto' }]
+    }
 });
 
 const multerupload = multer({ storage });
